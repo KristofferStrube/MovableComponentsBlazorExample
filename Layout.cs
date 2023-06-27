9c0e9e5ab5e71dd1b4cf57f76edcbb0265edf991
KristofferStrube <kristoffer.strube@gmail.com>
2023-06-27 18:44:33 +0200

    Added project

---
+++ Layout.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MovableComponentsBlazorExample;
+
+public class Layout : ComponentBase
+{
+    protected List<RenderFragment> children { get; set; } = new List<RenderFragment>();
+
+    [Parameter]
+    public RenderFragment ChildComponent { set => AddChildToEnd(value); }
+
+    public RenderFragment? FirstChild => children.FirstOrDefault();
+    public RenderFragment? LastChild => children.LastOrDefault();
+
+    public void AddChildToEnd(RenderFragment value)
+    {
+        children.Add(value);
+        StateHasChanged();
+    }
+
+    public void AddChildToStart(RenderFragment value)
+    {
+        children.Insert(0, value);
+        StateHasChanged();
+    }
+
+    public void RemoveChild(RenderFragment value)
+    {
+        children.Remove(value);
+        StateHasChanged();
+    }
+}
